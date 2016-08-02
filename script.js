40b90a31be2e7d3f77098a06e990d7c7ff6040c4
Brian Phan <brianphan88@gmail.com>
2016-08-02 11:32:59 -0700

    added location obj with lat and long

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 // GLOBAL VARIABLES
+$(document).ready(function(){
+    //create location object and get location (lat and long)
+    var locObj = new LocationObj();
+    var myLocation = locObj.getLocation();
+    console.log(myLocation.lat);
+    console.log(myLocation.long);
+    //to access lat and long use:
+    //myLocation.lat
+    //myLocation.long
 
-
-
+});
+function LocationObj(){
+    var myPosition = {
+        lat: null,
+        long: null,
+        status: null
+    };
+    var nav = navigator.geolocation;
+    nav.getCurrentPosition(success, failure);
+    function success(position){
+        myPosition.lat = position.coords.latitude;
+        myPosition.long = position.coords.longitude;
+        myPosition.status = true;
+    }
+    function failure(error){
+        myPosition.status = false;
+        myPosition.error = error;
+    }
+    this.getLocation = function(){
+        return myPosition;
+    }
+}
 
 
 
