82674b19b2d09ca7006353b0dcd40ac73fc9defe
Bill Leonhardt <iamleonhardt@gmail.com>
2016-08-02 09:50:18 -0700

    Starting Point

---
+++ script.js
@@ -0,0 +1,109 @@
+// GLOBAL VARIABLES
+
+
+
+
+
+
+
+
+
+// PAGE 1 - Date Choice
+
+
+
+
+
+
+
+
+
+
+
+
+// PAGE 2 - Date Buttons
+
+
+
+
+
+
+
+
+
+
+
+// PAGE 3 - Event Choices
+
+
+
+
+
+
+
+
+
+
+
+// PAGE 4  -  Events Buttons
+
+
+
+
+
+
+
+
+
+
+// Dinner
+
+
+
+
+
+// Cafe
+
+
+
+
+
+// Parks
+
+
+
+
+
+// Theaters
+
+
+
+
+
+// Malls
+
+
+
+
+
+// Museum
+
+
+
+
+
+// PAGE 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
